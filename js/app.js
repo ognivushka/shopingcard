7c78984a8db02fcb868d3d7d3bc4d36210d7a809
Valeria Markevich <v.markevichhh@gmail.com>
2023-01-05 14:50:40 +0100

    save and delite frome local storaje

--- js/app.js
+++ js/app.js
@@ -15,6 +15,8 @@ function loadEventListeners(){
    shopingCartContent.addEventListener('click', removeCourse)
    //clear card btn
    clearCartBtn.addEventListener('click', clearCard)
+   //doc is ready
+   document.addEventListener('DOMContentLoaded', getFromLocalStorage)
 }
 
 
@@ -73,16 +75,59 @@ function addIntoCart(course){
    saveIntoStorage(course)
 }
 
+//add the courses into the local storage
+function saveIntoStorage(course){
+   let courses = getCoursesFromStorage();
+
+   //add courses into the arr
+   courses.push(course);
+
+   //storage save onli strings! => JSON concert to string
+   localStorage.setItem('courses', JSON.stringify(courses))
+
+}
+
+//get the contant from the storage
+function getCoursesFromStorage(){
+
+   let courses;
+   //exist in storage?=>value, not exist?=>[]
+   if(localStorage.getItem('courses')===null){
+      courses =[];
+   }else{
+      courses = JSON.parse(localStorage.getItem('courses'));
+   }
+   return courses
+}
 
 //remove course frome the DOM
 function removeCourse(e){
 
+   let course, courseId;
+
+   //remove frome the DOM
    if(e.target.classList.contains('remove')){
       e.target.parentElement.parentElement.remove()
+      course = e.target.parentElement.parentElement;
+      courseId = course.querySelector('a').getAttribute('data-id');
    }
-
+   // console.log(courseId);
+   //remove from loc storage
+   removeCourseLocalStorage(courseId)
 }
-
+//remove frome loc storage
+function removeCourseLocalStorage(id){
+   //get data from the loc storage
+   let coursesLS = getCoursesFromStorage();
+   //loop throught the arr and find the index to remove
+   coursesLS.forEach(function(courseLS, index){
+      if(courseLS.id === id){
+         coursesLS.splice(index,1)
+      }
+   })
+   //add the rest of the arr
+   localStorage.setItem('courses', JSON.stringify(coursesLS))
+}
 
 //clears the shoping card with the button
 function clearCard(){
@@ -91,4 +136,38 @@ function clearCard(){
    while(shopingCartContent.firstChild){
       shopingCartContent.removeChild(shopingCartContent.firstChild)
    }
+
+   //clear frome loc storage
+   clearLocalStorage()
+}
+
+//clear the whole local storage
+function clearLocalStorage(){
+   localStorage.clear();
+}
+
+//loads when doc is ready and print courses into shopping cart
+
+function getFromLocalStorage(){
+   let coursesLS = getCoursesFromStorage();
+
+   //loop throught the courses and print into the card
+   coursesLS.forEach(function(course){
+      //create <tr>
+      const row = document.createElement('tr')
+      //print the content
+      row.innerHTML =`
+            <tr>
+               <td>
+               <img src="${course.image}" width=100px>
+               </td>
+               <td>${course.title}</td>
+               <td>${course.price}</td>
+               <td>
+               <a href="#" class ="remove" data-id="${course.id}">X</a>
+               </td>
+            </tr>
+      `;
+      shopingCartContent.appendChild(row);
+   })
 }
